e773560cb45daf7ce9109d2329bde24a27159a2f
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-19 12:48:48 -0300

    Update: add the practicalExercise method to practice what you have learned

--- helloWorld/src/main/java/com/todocodeacademy/helloWorld/controller/HelloController.java
+++ helloWorld/src/main/java/com/todocodeacademy/helloWorld/controller/HelloController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/") //si no agrego no funciona para spring 3.0.2 y java 17.0.5
+    /*@GetMapping("/") //si no agrego no funciona para spring 3.0.2 y java 17.0.5
     public String sayHello(){
         return "Hola Mundo! Esto es una prueba";
-    }
+    }*/
 
+    @GetMapping("/")
+    public String practicalExercise(){
+        return "Spring Boot es una herramienta para desarrollar APIs en Java";
+    }
 }
