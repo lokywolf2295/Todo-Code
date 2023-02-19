ae1f92c07070ae17f335331c80d284d1d2015626
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-19 12:37:55 -0300

    Update: create HelloController project whit sayHello method to test in browser

---
+++ helloWorld/src/main/java/com/todocodeacademy/helloWorld/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.todocodeacademy.helloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/") //si no agrego no funciona para spring 3.0.2 y java 17.0.5
+    public String sayHello(){
+        return "Hola Mundo! Esto es una prueba";
+    }
+
+}
