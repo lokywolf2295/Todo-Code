b1ebf3acf485f36e08efef2218d9fa0b92d1c4f0
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-20 10:37:14 -0300

    Update: Add GetMappingPathVariable project for practicing url and phat variable

---
+++ GetMappingPathVariable/src/main/java/com/todocodeacademy/GetMappingPathVariable/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.todocodeacademy.GetMappingPathVariable.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "Bye World!";
+    }
+}
