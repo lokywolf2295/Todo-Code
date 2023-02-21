1a5dab16830825792de755a238e3abaae701ed39
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-21 14:20:31 -0300

    Update: Add exerciseTwo method

--- GetMappingRequestParam/src/main/java/com/todocodeacademy/GetMappingRequestParam/controller/HelloController.java
+++ GetMappingRequestParam/src/main/java/com/todocodeacademy/GetMappingRequestParam/controller/HelloController.java
@@ -21,4 +21,22 @@ public class HelloController {
         int sum = num1 + num2;
         return "The sum of Number 1: " + num1 + " + Number 2: " + num2 + " is: " + sum;
     }
+
+    @GetMapping("/imc")
+    public String exerciseTwo(@RequestParam float weight ,
+                           @RequestParam float height){
+        float imc = weight / (height*height);
+        String status;
+        if(imc <= 18.5){
+            status = " Your Weigh is Underweight";
+        } else if (imc >= 18.6 && imc <= 24.9) {
+            status = " Your Weigh is Normal Weight";
+        }else {
+            status = " Your Weigh is Overweight";
+        }
+
+        return "The IMC of a person who weighs: " + weight + " KGms. and measures: " + height + " Mts. is: " + imc + status;
+
+        
+    }
 }
