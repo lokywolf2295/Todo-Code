592d89d2a86a514dbdbbd273093e5ac6b919ef61
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-21 12:42:57 -0300

    Update: Add PathVariables in sayBye method

--- GetMappingPathVariable/src/main/java/com/todocodeacademy/GetMappingPathVariable/controller/HelloController.java
+++ GetMappingPathVariable/src/main/java/com/todocodeacademy/GetMappingPathVariable/controller/HelloController.java
@@ -1,18 +1,21 @@
 package com.todocodeacademy.GetMappingPathVariable.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello World!";
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "Hello World! " + name;
     }
 
-    @GetMapping("/bye")
-    public String sayBye(){
-        return "Bye World!";
+    @GetMapping("/bye/{name}/{age}/{profession}")
+    public String sayBye(@PathVariable String name,
+                         @PathVariable int age,
+                         @PathVariable String profession){
+        return "Bye World! Name: " + name + " Age: " + age + " Profession: " + profession;
     }
 }
