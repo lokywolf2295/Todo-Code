404c40d4e3876b80455beba677359aa52e55e9f5
lokywolf2295 <matiasesek1995@gmail.com>
2023-02-22 22:49:07 -0300

    Update: Add ExerciseIntegratorN project to solve exercise

---
+++ EjercicioIntegradorN1/src/main/java/com/todocodeacademy/EjercicioIntegradorN1/EjercicioIntegradorN1Application.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.EjercicioIntegradorN1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioIntegradorN1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioIntegradorN1Application.class, args);
+	}
+
+}
