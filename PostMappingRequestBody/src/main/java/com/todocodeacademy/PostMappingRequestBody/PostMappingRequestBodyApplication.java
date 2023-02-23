0a47cf097cb58e4657e34b6ed93869e17b750787
Matias E. Carballo <95238399+lokywolf2295@users.noreply.github.com>
2023-02-23 14:09:26 -0300

    Update: Add PostMappingRequestBodyApplication project for practicing this whit postman

---
+++ PostMappingRequestBody/src/main/java/com/todocodeacademy/PostMappingRequestBody/PostMappingRequestBodyApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.PostMappingRequestBody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMappingRequestBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMappingRequestBodyApplication.class, args);
+	}
+
+}
