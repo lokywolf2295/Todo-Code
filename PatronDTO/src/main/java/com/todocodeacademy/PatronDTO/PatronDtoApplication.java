bf0e2995bf0b1d3187571b0a4bddc35e902c4632
lokywolf2295 <matiasesek1995@gmail.com>
2023-03-12 17:21:02 -0300

    Update: add PatronDTO project for DTO pattern practice

---
+++ PatronDTO/src/main/java/com/todocodeacademy/PatronDTO/PatronDtoApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.PatronDTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronDtoApplication.class, args);
+	}
+
+}
