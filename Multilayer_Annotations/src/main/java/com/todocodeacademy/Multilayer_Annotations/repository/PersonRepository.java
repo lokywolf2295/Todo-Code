29fc9d7e243877d82ca3b626b178fbab423b73c3
lokywolf2295 <matiasesek1995@gmail.com>
2023-03-14 21:36:45 -0300

    Update: add IPersonService class and edit PersonRepository and Service for annotations practice

--- Multilayer_Annotations/src/main/java/com/todocodeacademy/Multilayer_Annotations/repository/PersonRepository.java
+++ Multilayer_Annotations/src/main/java/com/todocodeacademy/Multilayer_Annotations/repository/PersonRepository.java
@@ -1,6 +1,11 @@
 
 package com.todocodeacademy.Multilayer_Annotations.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository /*Agregamos la anotación para informar que esta clase es de tipo repositorio*/
 public class PersonRepository {
+
+    //métodos para llamar a la base de datos, etc
 
 }
--- Multilayer_Annotations/src/main/java/com/todocodeacademy/Multilayer_Annotations/service/PersonService.java
+++ Multilayer_Annotations/src/main/java/com/todocodeacademy/Multilayer_Annotations/service/PersonService.java
@@ -1,5 +1,24 @@
 package com.todocodeacademy.Multilayer_Annotations.service;
 
-public class PersonService  {
+import com.todocodeacademy.Multilayer_Annotations.model.Person;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service /*Agregamos la anotación para informar que esta clase es de tipo servicio*/
+public class PersonService implements IPersonService {
+
+    @Override
+    public void createPerson(Person per) {
+        //lógica de creación
+        System.out.println("Persona Creada");
+    }
+
+    @Override
+    public List<Person> getPeople() {
+        //acá debería ir la lógica de devolver la lista de personas
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    //métodos de lógica de negocio
 }
