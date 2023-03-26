979b2ce1f3ed0043baf7294dda954e355e90ad9e
lokywolf2295 <matiasesek1995@gmail.com>
2023-03-26 20:03:19 -0300

    Update: add project InyeccionDeDependenciasAutowired to practice with autowired annotation

---
+++ InyeccionDeDependenciasAutowired/src/main/java/com/todocodeacademy/InyeccionDeDependenciasAutowired/InyeccionDeDependenciasAutowiredApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.InyeccionDeDependenciasAutowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyeccionDeDependenciasAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InyeccionDeDependenciasAutowiredApplication.class, args);
+	}
+
+}
