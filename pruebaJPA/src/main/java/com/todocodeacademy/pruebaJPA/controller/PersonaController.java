217cc06f349f14b66326a7bd0c08eeaef1813a42
lokywolf2295 <matiasesek1995@gmail.com>
2023-12-17 23:12:54 -0300

    Update: add all endpoints to PersonaController class

---
+++ pruebaJPA/src/main/java/com/todocodeacademy/pruebaJPA/controller/PersonaController.java
@@ -0,0 +1,44 @@
+package com.todocodeacademy.pruebaJPA.controller;
+
+import com.todocodeacademy.pruebaJPA.model.Persona;
+import com.todocodeacademy.pruebaJPA.service.IPersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PersonaController {
+
+    @Autowired
+    private IPersonaService personaService;
+
+    @GetMapping("/personas/traer")
+    public List<Persona> getPersonas(){
+        return personaService.getPersonas();
+    }
+
+    @PostMapping("/personas/crear")
+    public String savePersona(@RequestBody Persona persona){
+        personaService.savePersona(persona);
+        return "La Persona fue creada correctamente";
+    }
+
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        personaService.deletePersona(id);
+        return "La Persona fue eliminada correctamente";
+    }
+
+    @PutMapping("/personas/editar/{id_original}")
+    public Persona editPersona(@PathVariable Long id_original,
+                              @RequestParam(required = false, name = "id") Long nuevaId,
+                              @RequestParam(required = false, name = "nombre") String nuevoNombre,
+                              @RequestParam(required = false, name = "apellido") String nuevoApellido,
+                              @RequestParam(required = false, name = "edad") int nuevaEdad
+    ){
+        personaService.editPersona(id_original, nuevaId, nuevoNombre, nuevoApellido, nuevaEdad);
+        Persona persona = personaService.findPersona(nuevaId);
+        return persona;
+    }
+}
