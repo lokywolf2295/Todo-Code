c77b0bbfdbb95a1245ddd45e778c7fea69edf983
lokywolf2295 <matiasesek1995@gmail.com>
2024-01-23 17:28:12 -0300

    Update: add all methods to ICursoService interface

--- EstudianteProgramacion/src/main/java/com/todocodeacademy/estudianteprogramacion/service/ICursoService.java
+++ EstudianteProgramacion/src/main/java/com/todocodeacademy/estudianteprogramacion/service/ICursoService.java
@@ -1,4 +1,26 @@
 package com.todocodeacademy.estudianteprogramacion.service;
 
+import com.todocodeacademy.estudianteprogramacion.model.entity.Curso;
+
+import java.util.List;
+
 public interface ICursoService {
+
+//metodo para guardar un curso
+    public void createCurso(Curso curso);
+
+//metodo para Listar los cursos
+    public List<Curso> getCursos();
+
+    //metodo para encontrar los cursos que contengan una palabra
+    public List<Curso> findCurso(String palabra);
+
+    //metodo para encontrar un curso por id
+    public Curso findCursoById(Long id_curso);
+
+    //metodo para eliminar un curso por id
+    public void deleteCurso(Long id_curso);
+
+    //metodo para editar un curso
+    public void editCurso(Curso curso);
 }
