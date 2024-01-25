a331d881ac4a12eaf2cf4d9d93cf458b61b6c595
lokywolf2295 <matiasesek1995@gmail.com>
2024-01-24 21:26:36 -0300

    Update: add all methods to ITemaService interface

--- EstudianteProgramacion/src/main/java/com/todocodeacademy/estudianteprogramacion/service/ITemaService.java
+++ EstudianteProgramacion/src/main/java/com/todocodeacademy/estudianteprogramacion/service/ITemaService.java
@@ -1,4 +1,22 @@
 package com.todocodeacademy.estudianteprogramacion.service;
 
+import com.todocodeacademy.estudianteprogramacion.model.entity.Curso;
+import com.todocodeacademy.estudianteprogramacion.model.entity.Tema;
+
 public interface ITemaService {
+
+    //metodo para guardar un tema relacionado con un curso
+    public void createTema(Tema tema, Curso id_curso);
+
+    //metodo para encontrar los temas de un curso
+    public void findTemasByCurso(Curso id_curso);
+
+    //metodo para encontrar un tema por id
+    public void findTemaById(Long id_tema);
+
+    //metodo para eliminar un tema por id
+    public void deleteTema(Long id_tema);
+
+    //metodo para editar un tema
+    public void editTema(Tema tema);
 }
