49fe5a168661cbadb3fffd816d5d353704d94771
lokywolf2295 <matiasesek1995@gmail.com>
2024-02-02 00:51:34 -0300

    Update: modify ITemaService interface to improve some methods

--- EstudianteProgramacion/src/main/java/com/todocodeacademy/estudianteprogramacion/service/ITemaService.java
+++ EstudianteProgramacion/src/main/java/com/todocodeacademy/estudianteprogramacion/service/ITemaService.java
@@ -1,18 +1,22 @@
 package com.todocodeacademy.estudianteprogramacion.service;
 
-import com.todocodeacademy.estudianteprogramacion.model.entity.Curso;
 import com.todocodeacademy.estudianteprogramacion.model.entity.Tema;
+
+import java.util.List;
 
 public interface ITemaService {
 
     //metodo para guardar un tema relacionado con un curso
-    public void createTema(Tema tema, Curso id_curso);
+    public void createTema(Tema tema);
 
-    //metodo para encontrar los temas de un curso
-    public void findTemasByCurso(Curso id_curso);
+    //metodo para encontrar todos los temas
+    public List<Tema> getTemas();
 
     //metodo para encontrar un tema por id
-    public void findTemaById(Long id_tema);
+    public Tema findTemaById(Long id_tema);
+
+    //metodo para encontrar los temas de un curso
+    public List<Tema> findTemasByCurso(Long id_curso);
 
     //metodo para eliminar un tema por id
     public void deleteTema(Long id_tema);
